09f8c578fe16cadfa9e4b72fbf20836f1208ca25
kosa-kww <dyffh1031@gmail.com>
2023-09-16 22:07:36 +0900

    feat: 리액트 라우터 설정

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
-import './App.css';
+import { useSelector } from 'react-redux';
+import styles from './App.module.css';
 import Login from './pages/Login';
+import { selectUser } from './slices/userSlice';
+import { Outlet } from 'react-router-dom';
+import Sidebar from './features/sideBar/Sidebar';
+import Header from './features/header/Header';
 
 function App() {
-  return <Login />;
+  const currUser = useSelector(selectUser);
+  const isLoggedIn = currUser.value == null;
+  const drawerWidth = 240;
+
+  return isLoggedIn ? (
+    <Login />
+  ) : (
+    <div style={{ display: 'flex' }}>
+      <Sidebar drawerWidth={drawerWidth} />
+      <div className={styles.content}>
+        <Header />
+        <Outlet />
+      </div>
+    </div>
+  );
 }
 
 export default App;
