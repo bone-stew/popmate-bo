717d2463100b67e4f8493a83bce15d7af8192434
서명현 <azure394@gmail.com>
2023-09-25 03:47:48 +0900

    feat: 입장 처리 구현 및 예외 처리 (#61)
    * fix: 수정된 데이터로 업데이트되도록 수정
    
    * feat: 예약자 입장 API 변경
    
    * feat: 예약 번호 컬럼 추가
    
    * feat: QR 한 번 인식한 후 재인식 방지
    
    * feat: 입장 예외 처리 구현

--- src/pages/ReservationQR.jsx
+++ src/pages/ReservationQR.jsx
@@ -1,41 +1,85 @@
-import React  from 'react';
+import React, { useRef, useState } from 'react';
 import styles from '../features/admin/ReservationQr.module.css';
 import QrCodeReader, { QRCode } from 'react-qrcode-reader';
 import JsonAxios from '../api/jsonAxios';
+import EntryConfirmationDialog from '../features/dialog/EntryConfirmationDialog';
 
 function ReservationQR() {
+  const prevQRCode = useRef(null);
+  const [dialogOpen, setDialogOpen] = useState(false);
+  const [dialogMessage, setDialogMessage] = useState('');
+
+  // 다이얼로그 열기
+  const handleOpenDialog = () => {
+    setDialogOpen(true);
+    setDialogMessage('입장 처리가 완료되었습니다.');
+  };
+
+  const handleOpenFailDialog = () => {
+    setDialogOpen(true);
+  };
+
+  // 다이얼로그 닫기
+  const handleCloseDialog = () => {
+    setDialogOpen(false);
+    setDialogMessage(''); // 메시지 초기화
+  };
 
   const handleRead = (code: QRCode) => {
     const text = code.data;
+    if (text === prevQRCode.current) {
+      return; // 이미 인식한 QR 코드인 경우 처리 중지
+    }
+    prevQRCode.current = text;
     const keyValuePairs = text.split(',');
     const data = {};
     keyValuePairs.forEach((pair) => {
       const [key, value] = pair.split(':');
       data[key] = value;
     });
-    if(data.reservationId && data.userId){
+    if (data.reservationId && data.userId) {
       const reservationId = data.reservationId;
-      const url = `reservations/${reservationId}/entry`;
-      JsonAxios.patch(url).then((res) => {
-        const message = res.data.data;
-        alert(message);  
-      }).catch((error) => {
-        alert('입장 QR코드가 아닙니다.');
-      });
+      const userId = data.userId;
+
+      const apiUrl = `reservations/${reservationId}/entrance`;
+      const params = {
+        reservationUserId: userId,
+      };
+
+      JsonAxios.patch(apiUrl, params)
+        .then((res) => {
+          console.log(res.data);
+          if (res.data.code === 'SUCCESS') {
+            handleOpenDialog(); // 성공 시 다이얼로그 열기
+          } else {
+            setDialogMessage(res.data.data);
+            handleOpenFailDialog(); // 실패 시 경고창 표시
+          }
+        })
+        .catch((error) => {
+          setDialogMessage(error.response.data.message);
+          handleOpenFailDialog(); // 실패 시 경고창 표시
+          // alert('입장 QR코드가 아닙니다.');
+          console.error('API 호출 중 오류 발생:', error);
+          console.log(error.response.data.message);
+        });
     }
-  }
-  
+  };
+
   return (
-    <div className={styles.container}>
-      <div className={styles.title}>
-        <div className={styles.title1}>팝업스토어 입장 QR 코드</div>
-      </div>
-      <div className={styles.outer}>
-        <div style={{ width: '100%', height : '100%'}}>
-          <QrCodeReader delay={100} width={900} height={900} onRead={handleRead} />
+    <>
+      <div className={styles.container}>
+        <div className={styles.title}>
+          <div className={styles.title1}>팝업스토어 입장 QR 코드</div>
+        </div>
+        <div className={styles.outer}>
+          <div style={{ width: '100%', height: '100%' }}>
+            <QrCodeReader delay={100} width={900} height={900} onRead={handleRead} />
+          </div>
         </div>
       </div>
-    </div>
+      <EntryConfirmationDialog open={dialogOpen} onClose={handleCloseDialog} message={dialogMessage} />
+    </>
   );
 }
 
